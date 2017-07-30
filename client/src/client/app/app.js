23f7da9051846a5c38cb1069b37e17f972cc2bbe
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-30 23:30:39 +0200

    use characters

--- client/src/client/app/app.js
+++ client/src/client/app/app.js
@@ -17,6 +17,7 @@ function app() {
   swip.init({ socket: socket, container: document.getElementById('root') }, function (client) {
     assetsLoader.getInstance().onComplete = onComplete;
     assetsLoader.getInstance().addFile("atari400.png","ground");
+    assetsLoader.getInstance().addFile("character.png","character");
     assetsLoader.getInstance().load();
     var converter = client.converter;
     var stage = client.stage;
@@ -82,14 +83,14 @@ function app() {
         assetsManager.getInstance().addImage(data[alias],alias);
     }
     let atlas = new TextureAtlas();
-    atlas.data = assetsManager.getInstance().getImageByAlias("ground");
-    atlas.createTexture( "texture_1", 0,0,256,156);
+    atlas.data = assetsManager.getInstance().getImageByAlias("character");
+    atlas.createTexture( "texture_1", 0,0,136,130);
 
     let texture = atlas.getTextureByName("texture_1"); // on retrouve notre texture
     let bmp = new Bitmap(); // on créer un nouvel objet de type Bitmap
     bmp.texture = texture; // on y associe la texture
-    bmp.width = 256/2; // on définie la largeur
-    bmp.height = 156/1.5;//... puis la hauteur
+    bmp.width = 136; // on définie la largeur
+    bmp.height = 130;//... puis la hauteur
     bmp.x = 200;
     bmp.y = 200;
     character = bmp;
