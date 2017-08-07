55435390d3cd58c82f2173eb6cc2ea3f07ab92b8
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-07 20:48:28 +0200

    add constants

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,10 @@
 const DefaultWidthEnemy = 256/2;
 const DefaultHeightEnemy = 156/2;
-
+const DefaultWidthCharacter = 136/2;
+const DefaultHeightCharacter = 130/2;
 module.exports = {
   DefaultHeightEnemy,
-  DefaultWidthEnemy
+  DefaultWidthEnemy,
+  DefaultWidthCharacter,
+  DefaultHeightCharacter
 }
