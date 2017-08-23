9108d633b1f382c1f73eb323703cbd4662cd39fa
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-23 19:49:15 +0200

    add damages

--- server/index.js
+++ server/index.js
@@ -230,7 +230,7 @@ function updateGame(client, character, maze ) {
   newEnemies.map(enemy => {
     if(intersectRect(character, enemy))
     {
-      //newLife = newLife - 2;
+      newLife = newLife - 2;
     }
   });
 
