ed4dd972bb32f8ff57c85ab28132ce5aa262ab6a
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-29 18:51:07 +0200

    add sprite and its constants

--- client/src/client/app/constants.js
+++ client/src/client/app/constants.js
@@ -2,3 +2,10 @@ export const DefaultWidthEnemy = 136/2;
 export const DefaultHeightEnemy = 132/2;
 export const DefaultWidthCharacter = 136/2;
 export const DefaultHeightCharacter = 130/2;
+export const DefaultWidthMedikit = 136/4;
+export const DefaultHeightMedikit = 135/4;
+
+export const MaxEnemies = 2;
+export const MaxMedic = 1;
+export const MaxkillEnemiesItems = 1;
+export const MaxHoles = 2;
